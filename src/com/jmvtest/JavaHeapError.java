8c8f637e857dff4b658837c3a4d468e86e155941
NULL <lgsdaredevil@users.noreply.github.com>
2018-03-14 19:43:01 +0800

    Add files via upload

---
+++ src/com/jmvtest/JavaHeapError.java
@@ -0,0 +1,20 @@
+/**  
+* @Title: JavaHeapError.java  
+* @Package com.jmvtest  
+* @Description: TODO(用一句话描述该文件做什么)  
+* @author dared  
+* @date 2018年3月14日  
+* @version V1.0  
+*/ 
+package com.jmvtest;
+
+/**  
+* @ClassName: JavaHeapError  
+* @Description: 测试JVM的OutOfMemoryError异常  
+* @author lgs  
+* @date 2018年3月14日  
+*    
+*/
+public class JavaHeapError {
+
+}
