374c4da7dc682ab10c7f5d92032c1f66937273fe
dared <dared@DESKTOP-68LJKDD>
2018-03-14 20:05:38 +0800

    测试JVM的OutOfMemoryError异常

--- src/com/jmvtest/JavaHeapError.java
+++ src/com/jmvtest/JavaHeapError.java
@@ -8,13 +8,26 @@
 */ 
 package com.jmvtest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**  
 * @ClassName: JavaHeapError  
 * @Description: 测试JVM的OutOfMemoryError异常  
 * @author lgs  
 * @date 2018年3月14日  
-*    
+* 
+* VM Args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
 */
 public class JavaHeapError {
+	static class OOMObject{}
+	public static void main(String[] args) {
+		List<OOMObject> list = new ArrayList<OOMObject>();
+		
+		while (true) {
+			list.add(new OOMObject());
+		}
+		
+	}
 
 }
